bb477fff9d692b0e0f44001f12fcfe573a4db99e
Sachitha <sachitha@keeneye.solutions>
2018-08-20 11:14:09 +0530

    add file

---
+++ Canvas/CanvasHandler/CanvasView.swift
@@ -0,0 +1,21 @@
+//
+//  CanvasView.swift
+//  Canvas
+//
+//  Created by Sachitha on 8/20/18.
+//  Copyright © 2018 Sachitha. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
